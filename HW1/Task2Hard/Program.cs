318451dea35093f8d04181bbd74f30a15e9741d5
d.guryev <d.guryev@mail.ru>
2023-08-10 12:13:36 +0300

    Task2Hard

---
+++ HW1/Task2Hard/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 2 HARD по желанию Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
+456 -> 3
+0 -> 1
+89,126 -> 5
+0,001->4
+*/
+
+Console.WriteLine("Введите число");
+double d = Convert.ToDouble(Console.ReadLine());
+
+string s = d.ToString();
+int count = 0;
+
+foreach (char c in s)
+    if (Char.IsDigit(c)) count++;
+
+Console.WriteLine($"Количество цифр {count}");    
