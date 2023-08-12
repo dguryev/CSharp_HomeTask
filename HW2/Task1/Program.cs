e4db1016097d7956e81eeabf4a8f9953a4319f95
d.guryev <d.guryev@mail.ru>
2023-08-12 13:20:10 +0300

    Task1

---
+++ HW2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+задача 1 необязательная
+
+на входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+
+1,45 -> 1,5
+1 -> нет
+567,123 -> 57,123
+*/
+
+Console.WriteLine("Введите число");
+double d = Convert.ToDouble(Console.ReadLine());
+
+string s = d.ToString();
+
+if (s.Length < 2)
+{
+    Console.WriteLine("В числе меньше 2-х цифр");
+    return;
+}
+
+if (Char.IsDigit(s[1]))
+    s = s.Remove(1, 1);
+else
+    s = s.Remove(2, 1);
+
+Console.WriteLine(s);
