ebe1a94bdd94df04dece3d1b36a552ac63cc14d1
d.guryev <d.guryev@mail.ru>
2023-09-01 22:56:11 +0300

    hw4 task1

---
+++ HW4/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int Pow(int a, int b)
+{
+    int p = a;
+    for (int i = 2; i <= b; i++)
+        p *= a;
+
+    return p;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите степень");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результат {Pow(a, b)}");
