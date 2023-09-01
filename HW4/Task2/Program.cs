57b1c879bdd2ec0d43d4117b3d049abb61593ac4
d.guryev <d.guryev@mail.ru>
2023-09-01 23:04:29 +0300

    hw4 task2

---
+++ HW4/Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int sumNum(int a)
+{
+    int s = 0;
+
+    while (a > 0)
+    {
+        s += a % 10;
+        a /= 10;
+    }
+
+    return s;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {sumNum(a)}");
