dffad098ef3db78176560069dd0b4b115a2b5550
d.guryev <d.guryev@mail.ru>
2023-09-09 01:05:34 +0300

    Task34

--- HW5/Task34/Program.cs
+++ HW5/Task34/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] CreateArray(int n)
+{
+    int[] array = new int[n];
+    Random r = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+        array[i] = r.Next(100, 1000);
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write($"[{array[0]}");
+
+    for (int i = 1; i < array.Length; i++)
+        Console.Write($", {array[i]}");
+
+    Console.Write($"]");
+}
+
+int CountEven(int[] array)
+{
+    int count=0;
+
+    foreach(int item in array)
+        if (item % 2 == 0)
+            count++;
+
+    return count;
+}
+
+int ArraySize = 4;
+
+int [] array = CreateArray(ArraySize);
+PrintArray(array);
+
+int countEven = CountEven(array);
+
+Console.WriteLine($" -> {countEven}");
